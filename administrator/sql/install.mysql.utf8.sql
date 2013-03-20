46b74a945859804d5c0bdf50ce67977105ad6ee8
Benjamin Wöster <benjamin.woester@gmail.com>
2013-03-20 02:32:04 +0100

    make joomla installer like sql scripts

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `#__events_event` (
   `catid` int(11) NOT NULL DEFAULT '0',
   `title` varchar(255) NOT NULL,
   `time_start` datetime NOT NULL,
-  `description` text,
+  `description` mediumtext,
   `location` varchar(255) DEFAULT NULL,
   `time_end` datetime DEFAULT NULL,
   `meeting_place` varchar(255) DEFAULT NULL,
@@ -23,18 +23,4 @@ CREATE TABLE IF NOT EXISTS `#__events_event` (
   KEY `location` (`location`),
   KEY `meeting_place` (`meeting_place`),
   KEY `cancelled` (`cancelled`)
---
--- Can't specify fk on checked_out, because joomla core will set the column to
--- value "0" whenever a record is closed ("0" isn't a valid user).
--- @see "libraries/joomla/database/table.php:921" (JTable::checkIn())
--- 
--- FOREIGN KEY `fk_events_event_checked_out_users_id` (`checked_out`)
---    REFERENCES `#__users` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT,
---
--- Can't specify fk on catid, because joomla core will set the column to
--- value "0" if we select the components "root" category (which is sematically
--- the same as specifying "not categorized").
--- 
--- FOREIGN KEY `fk_events_event_catid_categories_id` (`catid`)
---  REFERENCES `#__categories` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT COLLATE=utf8_general_ci;
